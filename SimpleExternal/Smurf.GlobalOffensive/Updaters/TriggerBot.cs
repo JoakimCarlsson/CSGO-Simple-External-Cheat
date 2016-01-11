8647d41b20dc0c0fb675b2b1a54671a4ff070b2a
JoakimCarlsson <JoakimCarlsson1994@gmail.com>
2016-01-11 06:17:46 +0100

    Removed burst options from triggerbot for now, we do not really need it.

--- SimpleExternal/Smurf.GlobalOffensive/Updaters/TriggerBot.cs
+++ SimpleExternal/Smurf.GlobalOffensive/Updaters/TriggerBot.cs
@@ -13,14 +13,6 @@ namespace Smurf.GlobalOffensive.Updaters
         private bool _triggerAllies;
         private bool _triggerEnemies;
         private bool _spawnProtection;
-        private bool _triggerBurst;
-        private bool _triggerBurstRandom;
-        public bool _triggerShooting;
-        private int _burstCount;
-        private int _burstCountFired = 0;
-        private int _lastClip;
-        private int _burstMin;
-        private int _burstMax;
         private int _delayFirstShot;
         private int _delayShots;
         private WinAPI.VirtualKeyShort _triggerKey;
@@ -37,10 +29,6 @@ namespace Smurf.GlobalOffensive.Updaters
             _triggerEnemies = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Enemies");
             _triggerAllies = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Allies");
             _spawnProtection = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Spawn Protected");
-            _triggerBurst = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Burst Enabled");
-            _triggerBurstRandom = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Burst Randomize");
-            _burstMin = Smurf.Settings.GetInt(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Burst Shots Min");
-            _burstMax = Smurf.Settings.GetInt(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Burst Shots Max");
             _delayFirstShot = Smurf.Settings.GetInt(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Delay FirstShot");
             _delayShots = Smurf.Settings.GetInt(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Delay Shots");
 
