8f3ae0c74464480ad5720e64d07dd8e613ef0767
Kungen <Kungen@DESKTOP-0CVT0TO>
2016-03-23 10:11:18 -0700

    Revert "We now use Mouse_event for our RCS and not setting the view angels."
    This reverts commit a664c29d4f258750fd84ad80c8fca231ff737b64.

--- SimpleExternal/Smurf.GlobalOffensive/Updaters/Rcs.cs
+++ SimpleExternal/Smurf.GlobalOffensive/Updaters/Rcs.cs
@@ -9,6 +9,7 @@ namespace Smurf.GlobalOffensive.Updaters
     {
         #region Fields
 
+        private Vector3 _newViewAngels;
         public float MaxYaw, MaxPitch;
         public bool RcsEnabled;
         private int _rcsStart;
@@ -18,6 +19,7 @@ namespace Smurf.GlobalOffensive.Updaters
 
         #region Properties
 
+        public Vector3 ViewAngels { get; set; }
         public Vector3 LastPunch { get; set; }
 
         #endregion
@@ -47,10 +49,19 @@ namespace Smurf.GlobalOffensive.Updaters
                 if (Smurf.LocalPlayer.ShotsFired <= _rcsStart)
                     return;
 
-            var punch = Smurf.LocalPlayer.VecPunch - LastPunch;
-            Vector3 pixels = punch / (float)(0.22 * 0.28 * 1) * 20;
-            WinAPI.mouse_event((uint)0, (uint)pixels.Y, (uint)-pixels.X, 0, 0);
+            ViewAngels = Smurf.Memory.Read<Vector3>((IntPtr) (Smurf.ClientState + Offsets.ClientState.ViewAngles));
+            _newViewAngels = ViewAngels;
 
+            var punch = Smurf.LocalPlayer.VecPunch - LastPunch;
+            //Vector3 pixels = punch / (float)(0.22 * 0.28 * 1) * 20;
+            //WinAPI.mouse_event((uint)0, (uint)pixels.Y, (uint)-pixels.X, 0, 0);
+            if (punch.X != 0 || punch.Y != 0)
+            {
+                _newViewAngels.X -= punch.X * MaxYaw;
+                _newViewAngels.Y -= punch.Y * MaxPitch;
+                //pixels = angles / (m_yaw * ingamesensitivity * windowssensitivity);
+                SetViewAngles(_newViewAngels);
+            }
         }
 
         private void ReadSettïngs()
