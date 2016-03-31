b5c9ef3fa6e8b1e9cc05423c842572d519d043ea
JoakimCarlsson <JoakimCarlsson1994@gmail.com>
2016-03-31 15:54:05 +0200

    Claned out triggerbot because we'll rewrite it so it works on faceit.

--- SimpleExternal/Smurf.GlobalOffensive/Feauters/TriggerBot.cs
+++ SimpleExternal/Smurf.GlobalOffensive/Feauters/TriggerBot.cs
@@ -27,57 +27,7 @@ namespace Smurf.GlobalOffensive.Feauters
 
         public void Update()
         {
-            if (!Smurf.Objects.ShouldUpdate())
-                return;
-
-            ReadSettings();
-
-            if (!_triggerEnabled)
-                return;
-
-            if (Smurf.KeyUtils.KeyIsDown(_triggerKey))
-            {
-                var target = Smurf.LocalPlayer.Target;
-                if (target != null && ((_triggerAllies && target.Team == Smurf.LocalPlayer.Team) || (_triggerEnemies && target.Team != Smurf.LocalPlayer.Team)))
-                {
-                    if (!AimOntarget)
-                    {
-                        AimOntarget = true;
-                        _triggerLastTarget = DateTime.Now.Ticks;
-                    }
-                    else
-                    {
-                        if (
-                            !(new TimeSpan(DateTime.Now.Ticks - _triggerLastTarget).TotalMilliseconds >= _delayFirstShot))
-                            return;
-                        if (!(new TimeSpan(DateTime.Now.Ticks - _triggerLastShot).TotalMilliseconds >= _delayShots))
-                            return;
-
-                        _triggerLastShot = DateTime.Now.Ticks;
-
-                        if (_spawnProtection)
-                            if (target.GunGameImmune)
-                                return;
-
-                        if (_triggerDash)
-                            if (Smurf.LocalPlayer.Velocity > 1)
-                                return;
-
-                        if (_triggerZoomed)
-                        {
-                            if (Smurf.LocalPlayerWeapon.ZoomLevel <= 0)
-                            {
-                                return;
-                            }
-                        }
-
-                        Shoot();
-                    }
-                }
-            }
-            else
-                AimOntarget = false;
-
+            
         }
 
         private void ReadSettings()
@@ -85,9 +35,7 @@ namespace Smurf.GlobalOffensive.Feauters
             try
             {
                 _triggerEnabled = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Enabled");
-                _triggerKey =
-                    (WinAPI.VirtualKeyShort)
-                        Convert.ToInt32(Smurf.Settings.GetString(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Key"), 16);
+                _triggerKey = (WinAPI.VirtualKeyShort) Convert.ToInt32(Smurf.Settings.GetString(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Key"), 16);
                 _triggerEnemies = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Enemies");
                 _triggerAllies = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Allies");
                 _spawnProtection = Smurf.Settings.GetBool(Smurf.LocalPlayerWeapon.WeaponName, "Trigger Spawn Protected");
